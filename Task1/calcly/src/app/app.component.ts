5adf60a81b8385e6139a9213f8e070c876f005ef
M_Rabie <mohamed.rabie.abdelhameed@gmail.com>
2023-09-04 22:44:14 +0300

    task1: finish ui

---
+++ Task1/calcly/src/app/app.component.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  dot() {
+    throw new Error('Method not implemented.');
+  }
+  back() {
+    throw new Error('Method not implemented.');
+  }
+  clear() {
+    throw new Error('Method not implemented.');
+  }
+  equal() {
+    throw new Error('Method not implemented.');
+  }
+  add() {
+    throw new Error('Method not implemented.');
+  }
+  percent() {
+    throw new Error('Method not implemented.');
+  }
+  sub() {
+    throw new Error('Method not implemented.');
+  }
+  mul() {
+    throw new Error('Method not implemented.');
+  }
+  div() {
+    throw new Error('Method not implemented.');
+  }
+  num(arg0: number) {
+    throw new Error('Method not implemented.');
+  }
+  title = 'calcly';
+}
