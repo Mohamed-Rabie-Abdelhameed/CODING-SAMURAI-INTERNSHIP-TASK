40dda1ae74a35c47371c23e72bc66da67a7902b0
M_Rabie <mohamed.rabie.abdelhameed@gmail.com>
2023-09-04 23:24:26 +0300

    finish logic

--- Task1/calcly/src/app/app.component.ts
+++ Task1/calcly/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import * as math from 'mathjs';
 
 @Component({
   selector: 'app-root',
@@ -6,35 +7,87 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  currentInput: string = '';
+  result: string = '';
+
   dot() {
-    throw new Error('Method not implemented.');
+    const operands = this.currentInput.split(/[+\-*/]/);
+    const lastOperand = operands[operands.length - 1];
+    if (!lastOperand.includes('.')) {
+      this.currentInput += '.';
+    }
   }
   back() {
-    throw new Error('Method not implemented.');
+    this.currentInput = this.currentInput.slice(0, -1);
+  }
+
+  equal() {
+    try {
+      const rawResult = math.evaluate(this.currentInput);
+      if (typeof rawResult === 'number') {
+        this.result = rawResult.toFixed(4);
+        this.result = this.removeTrailingZeros(this.result);
+      } else {
+        this.result = rawResult;
+      }
+    } catch (error) {
+      this.result = 'Error';
+    }
+  }
+  operator(operator: string) {
+    this.currentInput += ' ' + operator + ' ';
+  }
+  num(value: string) {
+    if (
+      (value === '00' && this.currentInput.endsWith('0')) ||
+      (value === '00' && this.currentInput === '')
+    ) {
+      this.currentInput += '0';
+    } else {
+      this.currentInput += value;
+    }
   }
   clear() {
-    throw new Error('Method not implemented.');
+    this.currentInput = '';
+    this.result = '';
   }
-  equal() {
-    throw new Error('Method not implemented.');
+
+  @HostListener('document:keydown', ['$event'])
+  handleKeyboardEvent(event: KeyboardEvent) {
+    const key = event.key;
+
+    const operands = this.currentInput.split(/[+\-*/]/);
+
+    const lastOperand = operands[operands.length - 1];
+
+    if (this.isNumeric(key)) {
+      if (key === '.' && lastOperand.includes('.')) {
+        return;
+      }
+      this.num(key);
+    } else if (key === '+' || key === '-' || key === '*' || key === '/') {
+      this.operator(key);
+    } else if (key === 'Enter' || key === '=') {
+      this.equal();
+    } else if (key === 'Escape' || key === 'c' || key === 'C') {
+      this.clear();
+    } else if (key === 'Backspace') {
+      this.back();
+    } else if (key === '.') {
+      if (!lastOperand.includes('.')) {
+        this.dot();
+      }
+    }
   }
-  add() {
-    throw new Error('Method not implemented.');
+
+  private isNumeric(str: string): boolean {
+    return /^\d+$/.test(str) || str === '.';
   }
-  percent() {
-    throw new Error('Method not implemented.');
-  }
-  sub() {
-    throw new Error('Method not implemented.');
-  }
-  mul() {
-    throw new Error('Method not implemented.');
-  }
-  div() {
-    throw new Error('Method not implemented.');
-  }
-  num(arg0: number) {
-    throw new Error('Method not implemented.');
+
+  removeTrailingZeros(num: string): string {
+    const strNum = num.toString();
+    const stripped = strNum.replace(/(\.[0-9]*[1-9])0*|(\.0*)$/, '$1');
+    return stripped;
   }
   title = 'calcly';
 }
